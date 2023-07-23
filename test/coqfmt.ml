c10826f5dae9593abc11d4d446963fe36d6fdc99
Hiroki Tokunaga <tokusan441@gmail.com>
2023-07-23 14:06:53 +0900

    Add a test

--- test/coqfmt.ml
+++ test/coqfmt.ml
@@ -1,6 +1,20 @@
 let test_empty_case () =
   Alcotest.(check string) "same string" "" (Coqfmt.format "")
 
+let test_example () =
+  Alcotest.(check string)
+    "same string" "Example one_eq_one:1=1. Proof. reflexivity. Qed."
+    (Coqfmt.format {|Exampme one_eq_one: 1 = 1.
+Proof.
+  reflexivity.
+Qed.|})
+
 let () =
   Alcotest.run "Coqfmt"
-    [ ("format", [ Alcotest.test_case "Empty string" `Quick test_empty_case ]) ]
+    [
+      ( "format",
+        [
+          Alcotest.test_case "Empty string" `Quick test_empty_case;
+          Alcotest.test_case "Example" `Quick test_example;
+        ] );
+    ]
