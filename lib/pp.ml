a5499a96f3a6eee802f271675a81413d51799734
Hiroki Tokunaga <tokusan441@gmail.com>
2023-07-24 14:49:16 +0900

    unimplemented!

--- lib/pp.ml
+++ lib/pp.ml
@@ -1,7 +1,9 @@
+exception NotImplemented
+
 let pp_ast CAst.{ v = Vernacexpr.{ control = _; attrs = _; expr }; loc = _ } =
   match expr with
   | VernacDefinition _ -> "Example one_eq_one: 1 = 1."
   | VernacProof _ -> "Proof."
   | VernacExtend _ -> "  reflexivity."
   | VernacEndProof _ -> "Qed."
-  | _ -> ""
+  | _ -> raise NotImplemented
