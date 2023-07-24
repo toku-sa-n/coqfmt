6e2f63463cf3b20729085f41a59fcdcd48082906
Hiroki Tokunaga <tokusan441@gmail.com>
2023-07-24 14:47:57 +0900

    Split into files

--- lib/coqfmt.ml
+++ lib/coqfmt.ml
@@ -1,14 +1,7 @@
-let pp_ast CAst.{ v = Vernacexpr.{ control = _; attrs = _; expr }; loc = _ } =
-  match expr with
-  | VernacDefinition _ -> "Example one_eq_one: 1 = 1."
-  | VernacProof _ -> "Proof."
-  | VernacExtend _ -> "  reflexivity."
-  | VernacEndProof _ -> "Qed."
-  | _ -> ""
-
 (* Given that codes are usually stored in files, it is better to append a `\n` at the end if the code is not empty. *)
 let format = function
   | "" -> ""
   | x ->
       Init.init_coq ();
-      (Ast.generate_from_code x |> List.map pp_ast |> String.concat "\n") ^ "\n"
+      (Ast.generate_from_code x |> List.map Pp.pp_ast |> String.concat "\n")
+      ^ "\n"
---
+++ lib/pp.ml
@@ -0,0 +1,7 @@
+let pp_ast CAst.{ v = Vernacexpr.{ control = _; attrs = _; expr }; loc = _ } =
+  match expr with
+  | VernacDefinition _ -> "Example one_eq_one: 1 = 1."
+  | VernacProof _ -> "Proof."
+  | VernacExtend _ -> "  reflexivity."
+  | VernacEndProof _ -> "Qed."
+  | _ -> ""
