2b54f7e43aed65863fb846d7f0fc1af69dedcba0
Hiroki Tokunaga <tokusan441@gmail.com>
2023-07-27 21:43:12 +0900

    Merge the inner state

--- lib/printer.ml
+++ lib/printer.ml
@@ -1,9 +1,8 @@
-type inner = { buffer : Buffer.t }
-type t = inner ref
+type t = { buffer : Buffer.t }
 
 (* {{The doc} https://v2.ocaml.org/api/Buffer.html} says to allocate 16 buffers
    if unsure. *)
-let create () = ref { buffer = Buffer.create 16 }
-let write t = Buffer.add_string !t.buffer
-let newline t = Buffer.add_char !t.buffer '\n'
-let contents t = Buffer.contents !t.buffer
+let create () = { buffer = Buffer.create 16 }
+let write t = Buffer.add_string t.buffer
+let newline t = Buffer.add_char t.buffer '\n'
+let contents t = Buffer.contents t.buffer
