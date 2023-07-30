4f6ed1225cf905c187e4372405bd39a042a57a6e
Hiroki Tokunaga <tokusan441@gmail.com>
2023-07-30 17:53:17 +0900

    Read from stdin, format, and print out (#27)

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,11 @@
-let () = print_endline "Hello, World!"
+let () = Coqfmt.Init.init_coq ()
+
+let input =
+  let rec loop acc =
+    match read_line () with
+    | line -> loop (line :: acc)
+    | exception End_of_file -> List.rev acc |> String.concat "\n"
+  in
+  loop []
+
+let () = Coqfmt.Format.format input |> print_string
