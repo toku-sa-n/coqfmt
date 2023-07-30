4c2b543f034f67c048c40c5fe98b246b77b97e80
Hiroki Tokunaga <tokusan441@gmail.com>
2023-07-30 14:12:01 +0900

    Simplify

--- lib/ast.mli
+++ lib/ast.mli
@@ -1,7 +1,5 @@
 type t = Vernacexpr.vernac_control list
 (** The type of AST. *)
 
-val generate_from_code : string -> Vernacexpr.vernac_control list
-(** Generates the AST of the given source code. Unlike normal ASTs like Haskell's
-   one, the generated AST is composed of sub-ASTs, and each element corresponds
-   to a sentence separated by a dot (`.`).*)
+val generate_from_code : string -> t
+(** Generates the AST of the given source code. *)
