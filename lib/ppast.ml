897e4841c30c5c3b46a68fa7de002c6daa15edae
Hiroki Tokunaga <tokusan441@gmail.com>
2023-07-31 14:31:59 +0900

    Remove unnecessary branches

--- lib/ppast.ml
+++ lib/ppast.ml
@@ -30,23 +30,6 @@ let pp_prim_token printer = function
 
 let rec pp_constr_expr printer CAst.{ v; loc = _ } =
   match v with
-  | Constrexpr.CRef (_, None) -> raise NotImplemented
-  | Constrexpr.CFix _ -> raise NotImplemented
-  | Constrexpr.CCoFix _ -> raise NotImplemented
-  | Constrexpr.CProdN _ -> raise NotImplemented
-  | Constrexpr.CLambdaN _ -> raise NotImplemented
-  | Constrexpr.CLetIn _ -> raise NotImplemented
-  | Constrexpr.CAppExpl _ -> raise NotImplemented
-  | Constrexpr.CApp _ -> raise NotImplemented
-  | Constrexpr.CProj _ -> raise NotImplemented
-  | Constrexpr.CRecord _ -> raise NotImplemented
-  | Constrexpr.CCases _ -> raise NotImplemented
-  | Constrexpr.CLetTuple _ -> raise NotImplemented
-  | Constrexpr.CIf _ -> raise NotImplemented
-  | Constrexpr.CHole _ -> raise NotImplemented
-  | Constrexpr.CPatVar _ -> raise NotImplemented
-  | Constrexpr.CEvar _ -> raise NotImplemented
-  | Constrexpr.CSort _ -> raise NotImplemented
   | Constrexpr.CNotation
       (None, (InConstrEntry, init_notation), (init_replacers, [], [], [])) ->
       let rec loop notation replacers =
