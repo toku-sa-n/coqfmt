3d8a1a3138b108cc17a572210a9f8268b4c27aff
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-01 16:31:09 +0900

    Narrowize

--- lib/ppast.ml
+++ lib/ppast.ml
@@ -69,7 +69,7 @@ let pp_subast printer
       Printer.write printer ": ";
       pp_definition_expr printer expr;
       Printer.write printer "."
-  | VernacStartTheoremProof (kind, [ ((ident, None), (_, _)) ]) ->
+  | VernacStartTheoremProof (kind, [ ((ident, None), ([], _)) ]) ->
       pp_theorem_kind printer kind;
       Printer.write printer " ";
       pp_lident printer ident;
