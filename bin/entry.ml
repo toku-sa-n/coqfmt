aaafe91c5d1b2981caa60ce3ff3b2a0bebeef2e7
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-02 21:55:23 +0900

    Implement command-line parameters (#40)

---
+++ bin/entry.ml
@@ -0,0 +1,11 @@
+let input () =
+  let rec loop acc =
+    match read_line () with
+    | line -> loop (line :: acc)
+    | exception End_of_file -> List.rev acc |> String.concat "\n"
+  in
+  loop []
+
+let coqfmt () =
+  Coqfmt.Init.init_coq ();
+  input () |> Coqfmt.Format.format |> print_string
