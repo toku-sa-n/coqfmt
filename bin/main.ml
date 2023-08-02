b081223044b02c701b00db0b4ffce28c7a2890bd
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-02 19:42:27 +0900

    Split

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,3 @@
-open Cmdliner
-
 let format () =
   let input () =
     let rec loop acc =
@@ -14,24 +12,4 @@ let format () =
   let () = Coqfmt.Init.init_coq () in
   input () |> Coqfmt.Format.format |> print_string
 
-let cmd handler =
-  let doc = "Format the given Coq source code in a uniform style" in
-  let man =
-    [
-      `S Manpage.s_description;
-      `P
-        "The $(b,coqfmt) utility formats the given Coq source code in a \
-         uniform style. It reads from the standard input and writes to the \
-         standard output.";
-    ]
-  in
-  let version =
-    match Build_info.V1.version () with
-    | None -> failwith "No version found"
-    | Some v -> Build_info.V1.Version.to_string v
-  in
-  let info = Cmd.info "coqfmt" ~version ~doc ~man in
-  let term = Cmdliner.Term.(const handler $ const ()) in
-  Cmd.v info term
-
-let () = exit (Cmd.eval (cmd format))
+let () = exit (Cmdliner.Cmd.eval (Args.cmd format))
