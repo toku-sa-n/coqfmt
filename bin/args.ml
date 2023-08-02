c62d6b8f7add22cca8ed5efb8770e4464ef649be
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-02 19:43:34 +0900

    Split

--- bin/args.ml
+++ bin/args.ml
@@ -1,21 +1,21 @@
 open Cmdliner
 
-let cmd handler =
-  let doc = "Format the given Coq source code in a uniform style" in
-  let man =
-    [
-      `S Manpage.s_description;
-      `P
-        "The $(b,coqfmt) utility formats the given Coq source code in a \
-         uniform style. It reads from the standard input and writes to the \
-         standard output.";
-    ]
-  in
-  let version =
-    match Build_info.V1.version () with
-    | None -> failwith "No version found"
-    | Some v -> Build_info.V1.Version.to_string v
-  in
-  let info = Cmd.info "coqfmt" ~version ~doc ~man in
-  let term = Cmdliner.Term.(const handler $ const ()) in
-  Cmd.v info term
+let version =
+  match Build_info.V1.version () with
+  | None -> failwith "No version found"
+  | Some v -> Build_info.V1.Version.to_string v
+
+let doc = "Format the given Coq source code in a uniform style"
+
+let man =
+  [
+    `S Manpage.s_description;
+    `P
+      "The $(b,coqfmt) utility formats the given Coq source code in a uniform \
+       style. It reads from the standard input and writes to the standard \
+       output.";
+  ]
+
+let info = Cmd.info "coqfmt" ~version ~doc ~man
+let term handler = Cmdliner.Term.(const handler $ const ())
+let cmd handler = Cmd.v info (term handler)
