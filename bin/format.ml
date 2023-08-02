5da9083e470be0e07119676cc553b99ea6b4579e
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-02 19:57:01 +0900

    Split

---
+++ bin/format.ml
@@ -0,0 +1,13 @@
+let input () =
+  let rec loop acc =
+    match read_line () with
+    | line -> loop (line :: acc)
+    | exception End_of_file -> List.rev acc |> String.concat "\n"
+  in
+  loop []
+
+let format () =
+  (* Do not call `Coqfmt.Init.init_coq` before `Cmd.eval`, otherwise everything
+     (including help page, etc.) will be overwritten by Coq's ones. *)
+  let () = Coqfmt.Init.init_coq () in
+  input () |> Coqfmt.Format.format |> print_string
--- bin/main.ml
+++ bin/main.ml
@@ -1,15 +1,1 @@
-let format () =
-  let input () =
-    let rec loop acc =
-      match read_line () with
-      | line -> loop (line :: acc)
-      | exception End_of_file -> List.rev acc |> String.concat "\n"
-    in
-    loop []
-  in
-  (* Do not call `Coqfmt.Init.init_coq` before `Cmd.eval`, otherwise everything
-     (including help page, etc.) will be overwritten by Coq's ones. *)
-  let () = Coqfmt.Init.init_coq () in
-  input () |> Coqfmt.Format.format |> print_string
-
-let () = exit (Cmdliner.Cmd.eval (Args.cmd format))
+let () = exit (Cmdliner.Cmd.eval (Args.cmd Format.format))
