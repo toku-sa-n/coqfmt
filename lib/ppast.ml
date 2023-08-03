47ba4a146bda4ff1dfd3deee6be700a78975ecc9
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-03 13:07:19 +0900

    `open Printer` (#43)

--- lib/ppast.ml
+++ lib/ppast.ml
@@ -1,9 +1,11 @@
+open Printer
+
 exception NotImplemented
 
-let pp_id printer id = Names.Id.to_string id |> Printer.write printer
+let pp_id printer id = Names.Id.to_string id |> write printer
 
 let pp_lident printer CAst.{ v; loc = _ } =
-  Names.Id.to_string v |> Printer.write printer
+  Names.Id.to_string v |> write printer
 
 let pp_lname printer CAst.{ v; loc = _ } =
   match v with
@@ -11,15 +13,15 @@ let pp_lname printer CAst.{ v; loc = _ } =
   | Names.Anonymous -> raise NotImplemented
 
 let pp_definition_object_kind printer = function
-  | Decls.Example -> Printer.write printer "Example"
-  | Decls.Definition -> Printer.write printer "Definition"
+  | Decls.Example -> write printer "Example"
+  | Decls.Definition -> write printer "Definition"
   | _ -> raise NotImplemented
 
 let pp_sign printer = function
   | NumTok.SPlus -> ()
-  | NumTok.SMinus -> Printer.write printer "-"
+  | NumTok.SMinus -> write printer "-"
 
-let pp_unsigned printer n = Printer.write printer (NumTok.Unsigned.sprint n)
+let pp_unsigned printer n = write printer (NumTok.Unsigned.sprint n)
 
 let pp_signed printer (sign, n) =
   pp_sign printer sign;
@@ -27,12 +29,11 @@ let pp_signed printer (sign, n) =
 
 let pp_prim_token printer = function
   | Constrexpr.Number n -> pp_signed printer n
-  | Constrexpr.String s -> Printer.write printer s
+  | Constrexpr.String s -> write printer s
 
 let rec pp_constr_expr printer CAst.{ v; loc = _ } =
   match v with
-  | Constrexpr.CRef (id, None) ->
-      Libnames.string_of_qualid id |> Printer.write printer
+  | Constrexpr.CRef (id, None) -> Libnames.string_of_qualid id |> write printer
   | Constrexpr.CNotation
       (None, (InConstrEntry, init_notation), (init_replacers, [], [], [])) ->
       let rec loop notation replacers =
@@ -43,7 +44,7 @@ let rec pp_constr_expr printer CAst.{ v; loc = _ } =
             pp_constr_expr printer h;
             loop (String.sub s 1 (String.length s - 1)) t
         | s, _ ->
-            Printer.write printer (String.sub s 0 1);
+            write printer (String.sub s 0 1);
             loop (String.sub s 1 (String.length s - 1)) replacers
       in
       loop init_notation init_replacers
@@ -52,35 +53,35 @@ let rec pp_constr_expr printer CAst.{ v; loc = _ } =
 
 let pp_local_binder_expr printer = function
   | Constrexpr.CLocalAssum ([ name ], Constrexpr.Default Explicit, ty) ->
-      Printer.write printer "(";
+      write printer "(";
       pp_lname printer name;
-      Printer.write printer ": ";
+      write printer ": ";
       pp_constr_expr printer ty;
-      Printer.write printer ")"
+      write printer ")"
   | _ -> raise NotImplemented
 
 let pp_definition_expr printer = function
   | Vernacexpr.ProveBody ([], expr) ->
-      Printer.write printer ": ";
+      write printer ": ";
       pp_constr_expr printer expr
   | Vernacexpr.DefineBody ([ arg ], None, def_body, Some return_ty) ->
-      Printer.space printer;
+      space printer;
       pp_local_binder_expr printer arg;
-      Printer.write printer " : ";
+      write printer " : ";
       pp_constr_expr printer return_ty;
-      Printer.write printer " :=";
-      Printer.newline printer;
-      Printer.increase_indent printer;
+      write printer " :=";
+      newline printer;
+      increase_indent printer;
       pp_constr_expr printer def_body;
-      Printer.decrease_indent printer
+      decrease_indent printer
   | _ -> raise NotImplemented
 
 let pp_proof_end printer = function
-  | Vernacexpr.Proved (Vernacexpr.Opaque, None) -> Printer.write printer "Qed."
+  | Vernacexpr.Proved (Vernacexpr.Opaque, None) -> write printer "Qed."
   | _ -> raise NotImplemented
 
 let pp_theorem_kind printer = function
-  | Decls.Theorem -> Printer.write printer "Theorem"
+  | Decls.Theorem -> write printer "Theorem"
   | _ -> raise NotImplemented
 
 let pp_subast printer
@@ -88,20 +89,20 @@ let pp_subast printer
   match expr with
   | VernacDefinition ((NoDischarge, kind), (name, None), expr) ->
       pp_definition_object_kind printer kind;
-      Printer.space printer;
+      space printer;
       pp_lname printer name;
       pp_definition_expr printer expr;
-      Printer.write printer "."
+      write printer "."
   | VernacStartTheoremProof (kind, [ ((ident, None), ([], expr)) ]) ->
       pp_theorem_kind printer kind;
-      Printer.write printer " ";
+      write printer " ";
       pp_lident printer ident;
-      Printer.write printer ": ";
+      write printer ": ";
       pp_constr_expr printer expr;
-      Printer.write printer "."
+      write printer "."
   | VernacProof (None, None) ->
-      Printer.write printer "Proof.";
-      Printer.increase_indent printer
+      write printer "Proof.";
+      increase_indent printer
   | VernacInductive (Inductive_kw, inductives) ->
       let pp_single_inductive = function
         | ( ( (Vernacexpr.NoCoercion, (name, None)),
@@ -110,47 +111,46 @@ let pp_subast printer
               Vernacexpr.Constructors constructors ),
             [] ) ->
             pp_lident printer name;
-            if Option.has_some ty then Printer.write printer ": Type";
-            Printer.write printer " :=";
-            Printer.increase_indent printer;
+            if Option.has_some ty then write printer ": Type";
+            write printer " :=";
+            increase_indent printer;
             List.iter
               (fun (_, (name, _)) ->
-                Printer.newline printer;
-                Printer.write printer "| ";
+                newline printer;
+                write printer "| ";
                 pp_lident printer name)
               constructors;
-            Printer.decrease_indent printer
+            decrease_indent printer
         | _ -> raise NotImplemented
       in
       List.iteri
         (fun i inductive ->
           match i with
           | 0 ->
-              Printer.write printer "Inductive ";
+              write printer "Inductive ";
               pp_single_inductive inductive
           | _ ->
-              Printer.newline printer;
-              Printer.write printer "with ";
+              newline printer;
+              write printer "with ";
               pp_single_inductive inductive)
         inductives;
-      Printer.write printer "."
+      write printer "."
   (* FIXME: I have no idea how to extract the complete information of a `VernacExtend`.
      See https://stackoverflow.com/questions/76792174/how-to-extract-the-exact-information-of-genarg. *)
   | VernacExtend _ ->
-      Ppvernac.pr_vernac_expr expr
-      |> Pp.string_of_ppcmds |> Printer.write printer;
-      Printer.write printer "."
+      Ppvernac.pr_vernac_expr expr |> Pp.string_of_ppcmds |> write printer;
+      write printer "."
   | VernacEndProof proof_end ->
-      Printer.decrease_indent printer;
+      decrease_indent printer;
       pp_proof_end printer proof_end
   | _ -> raise NotImplemented
 
 (* Given that codes are usually stored in files, it is better to append a `\n` at the end if the code is not empty. *)
 let pp_ast ast =
-  let printer = Printer.create () in
+  let printer = create () in
   List.iter
     (fun subast ->
       pp_subast printer subast;
-      Printer.newline printer)
+      newline printer)
     ast;
-  Printer.contents printer
+  contents printer
