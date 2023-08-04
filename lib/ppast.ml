3839d49a33023435da94928dcbe51f6b952f031e
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-04 15:58:24 +0900

    Replace a wildcard

--- lib/ppast.ml
+++ lib/ppast.ml
@@ -37,7 +37,9 @@ let rec pp_cases_pattern_expr printer CAst.{ v; loc = _ } =
 
 and pp_cases_pattern_expr_r printer = function
   | Constrexpr.CPatAtom (Some id) -> pp_qualid printer id
-  | Constrexpr.CPatCstr (_, None, _) -> write printer "S n'"
+  | Constrexpr.CPatCstr (outer, None, _) ->
+      pp_qualid printer outer;
+      write printer " n'"
   | _ -> raise NotImplemented
 
 let rec pp_constr_expr printer CAst.{ v; loc = _ } = pp_constr_expr_r printer v
