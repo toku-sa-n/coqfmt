40e770bf6a853456ce0049a65734bcd7dcd17d75
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-07 12:14:17 +0900

    Replace a wildcard

--- lib/ppast.ml
+++ lib/ppast.ml
@@ -128,7 +128,7 @@ and pp_constr_expr_r printer = function
       loop init_notation init_replacers
   | Constrexpr.CPrim prim -> pp_prim_token printer prim
   | Constrexpr.CProdN (_, _) -> write printer " (x: foo) (y: foo)"
-  | Constrexpr.CHole (None, IntroAnonymous, _) -> ()
+  | Constrexpr.CHole (None, IntroAnonymous, None) -> ()
   | _ -> raise (NotImplemented (contents printer))
 
 and pp_case_expr printer = function
