65409aad277121c526565aaba5dd664cf9743d1c
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-11 22:43:06 +0900

    Fix indents

--- test/coq_files/tactics/destruct/nested-bullets/out.v
+++ test/coq_files/tactics/destruct/nested-bullets/out.v
@@ -2,9 +2,9 @@ Theorem or_comm : forall (x y : bool), orb x y = orb y x.
 Proof.
   destruct x.
     - destruct y.
-      + reflexivity.
-      + reflexivity.
+        + reflexivity.
+        + reflexivity.
     - destruct y.
-      + reflexivity.
-      + reflexivity.
+        + reflexivity.
+        + reflexivity.
 Qed.
