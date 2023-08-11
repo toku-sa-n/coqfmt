b50aa6312f44c72907e75e0534b6250d0b031b89
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-11 11:33:52 +0900

    Destruct a wildcard

--- lib/ppast.ml
+++ lib/ppast.ml
@@ -283,8 +283,8 @@ let pp_induction_clause printer = function
   | ( arg,
       ( None,
         Some
-          (Locus.ArgArg CAst.{ v = Tactypes.IntroOrPattern [ []; _ ]; loc = _ })
-      ),
+          (Locus.ArgArg
+            CAst.{ v = Tactypes.IntroOrPattern [ []; [ _ ] ]; loc = _ }) ),
       None ) ->
       pp_destruction_arg printer arg;
       write printer " as [| n']"
