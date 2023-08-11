23b077559fe534503113106371e1e591c6677997
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-11 21:32:27 +0900

    Add a test

---
+++ test/coq_files/tactics/destruct/nested-bullets/out.v
@@ -0,0 +1,10 @@
+Theorem or_comm : forall (x y : bool) : orb x y = orb y x.
+Proof.
+  destruct x.
+    - destruct y.
+      + reflexivity.
+      + reflexivity.
+    - destruct y.
+      + reflexivity.
+      + reflexivity.
+Qed.
