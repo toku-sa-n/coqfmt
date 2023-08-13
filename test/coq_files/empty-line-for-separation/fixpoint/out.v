d2937a1ad6d7be61c6474bfcb3c833a2127dddc5
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-13 15:59:14 +0900

    Add a test

---
+++ test/coq_files/empty-line-for-separation/fixpoint/out.v
@@ -0,0 +1,11 @@
+Fixpoint inc (n : nat) : nat :=
+  match n with
+  | O => S O
+  | S n' => S (inc n')
+  end.
+
+Fixpoint inc' (n : nat) : nat :=
+  match n with
+  | O => S O
+  | S n' => S (inc' n')
+  end.
