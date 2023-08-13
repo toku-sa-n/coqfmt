e25bc5cccc617b5b3d56eb0d83e3d536c0cdd85d
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-13 13:35:22 +0900

    Add a test

--- test/coq_files/module/in.v
+++ test/coq_files/module/in.v
@@ -1,1 +1,1 @@
-Module Foo. Definition x:nat:=3. End Foo.
+Module Foo. Definition x:nat:=3. End Foo. Module Bar. Definition x:nat=5 End.
--- test/coq_files/module/out.v
+++ test/coq_files/module/out.v
@@ -2,3 +2,8 @@ Module Foo.
   Definition x : nat :=
     3.
 End Foo.
+
+Module Bar.
+  Definition x : nat :=
+    5.
+End Bar.
