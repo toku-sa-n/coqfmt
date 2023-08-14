16147cf77b3a61aeff0f7eddff62dc7e8595bab6
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-14 22:50:39 +0900

    Destruct a wildcard

--- lib/ppast.ml
+++ lib/ppast.ml
@@ -83,7 +83,8 @@ let pp_sort_expr printer = function
   | Glob_term.UAnonymous { rigid = true } -> write printer "Type"
   | Glob_term.UAnonymous { rigid = false } ->
       raise (NotImplemented (contents printer))
-  | Glob_term.UNamed _ -> write printer "Set"
+  | Glob_term.UNamed [ _ ] -> write printer "Set"
+  | _ -> raise (NotImplemented (contents printer))
 
 let rec pp_constr_expr printer CAst.{ v; loc = _ } = pp_constr_expr_r printer v
 
