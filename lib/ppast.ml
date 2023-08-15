1e6c84051626a081663fd40db86bbe6b1ee3d687
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-15 17:54:07 +0900

    Eta reduction

--- lib/ppast.ml
+++ lib/ppast.ml
@@ -293,10 +293,10 @@ let pp_syntax_modifier = function
       concat [ write "at level "; write (string_of_int level) ]
   | _ -> fun printer -> raise (NotImplemented (contents printer))
 
-let pp_gen_tactic_arg (expr : Tacexpr.raw_tactic_arg) printer =
+let pp_gen_tactic_arg (expr : Tacexpr.raw_tactic_arg) =
   match expr with
-  | Tacexpr.TacCall ast -> concat [ pp_qualid (fst ast.v); write "." ] printer
-  | _ -> raise (NotImplemented (contents printer))
+  | Tacexpr.TacCall ast -> concat [ pp_qualid (fst ast.v); write "." ]
+  | _ -> fun printer -> raise (NotImplemented (contents printer))
 
 let pp_raw_red_expr printer (expr : Tacexpr.raw_red_expr) =
   match expr with
