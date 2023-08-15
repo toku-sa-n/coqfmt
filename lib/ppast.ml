bdc67531bf84d1e283db11eabfb5ea24cf3466db
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-15 22:36:52 +0900

    Use `function`

--- lib/ppast.ml
+++ lib/ppast.ml
@@ -340,8 +340,7 @@ and pp_intro_pattern_action_expr = function
   | Tactypes.IntroOrAndPattern expr -> pp_or_and_intro_pattern_expr expr
   | _ -> fun printer -> raise (NotImplemented (contents printer))
 
-and pp_intro_pattern_expr expr =
-  match expr with
+and pp_intro_pattern_expr = function
   | Tactypes.IntroAction expr -> pp_intro_pattern_action_expr expr
   | Tactypes.IntroNaming expr -> pp_intro_pattern_naming_expr expr
   | _ -> fun printer -> raise (NotImplemented (contents printer))
