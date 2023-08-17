52a4f0f148015f93b504c405d278c7ca21fb4b8e
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-17 15:39:58 +0900

    Destruct a wildcard

--- lib/ppast.ml
+++ lib/ppast.ml
@@ -258,7 +258,7 @@ let pp_fixpoint_expr = function
       } ->
       let pp_return_type =
         match rtype.v with
-        | Constrexpr.CHole _ -> nop
+        | Constrexpr.CHole (_, _, _) -> nop
         | _ -> sequence [ write " : "; pp_constr_expr rtype ]
       in
       sequence
