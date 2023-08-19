297332a06fdc240d0ad0770e2218be9005b7d142
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-19 14:32:59 +0900

    Use Coq's commandline interface (#128)

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,11 @@
-let () = exit (Cmdliner.Cmd.eval (Args.cmd Entry.coqfmt))
+let input () =
+  let rec loop acc =
+    match read_line () with
+    | line -> loop (line :: acc)
+    | exception End_of_file -> List.rev acc |> String.concat "\n"
+  in
+  loop []
+;;
+
+Coqfmt.Init.init_coq ();
+input () |> Coqfmt.Format.format |> print_string
