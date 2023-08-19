cf301df673e5d7f8b880a3f5bd90a4604025f9f9
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-19 13:38:18 +0900

    Fix the pass issue

--- test/require-test/test.sh
+++ test/require-test/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-coqc -Q . X foo.v
-OCAMLRUNPARAM=b dune exec ../../bin/main.exe -- -Q ../../test/require-test X bar.v < bar.v > bar.output.v 
+opam exec -- coqc -Q . X foo.v
+OCAMLRUNPARAM=b opam exec -- dune exec ../../bin/main.exe -- -Q ../../test/require-test X bar.v < bar.v > bar.output.v 
 diff bar.formatted.v bar.output.v
