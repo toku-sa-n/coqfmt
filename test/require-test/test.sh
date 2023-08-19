4555a8709abb1b74a05b89da8547fb3d526ab4d6
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-19 14:04:21 +0900

    Fix

--- test/require-test/test.sh
+++ test/require-test/test.sh
@@ -3,6 +3,6 @@
 opam exec -- coqc -Q . X Foo.v
 
 # Note that the working directory of the following command is
-# `_build/default/test/`.
+# `_build/default/test/require-test`.
 OCAMLRUNPARAM=b opam exec -- dune exec ../../bin/main.exe -- -Q ../../test/require-test X < Bar.v > Bar.output.v 
 diff Bar.formatted.v Bar.output.v
