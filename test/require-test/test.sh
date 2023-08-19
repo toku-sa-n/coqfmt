2380e9cccecad7cbe0b85d8cf68253fde75be89a
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-19 13:55:24 +0900

    Add a note

--- test/require-test/test.sh
+++ test/require-test/test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 opam exec -- coqc -Q . X foo.v
+
+# Note that the working directory of the following command is
+# `_build/default/test/`.
 OCAMLRUNPARAM=b opam exec -- dune exec ../../bin/main.exe -- -Q ../../test/require-test X bar.v < bar.v > bar.output.v 
 diff bar.formatted.v bar.output.v
