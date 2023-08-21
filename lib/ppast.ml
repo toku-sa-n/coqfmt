b4ceda71a3dffa67286bdd9816b397203d2afee3
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-21 13:26:37 +0900

    Pass the test

--- lib/ppast.ml
+++ lib/ppast.ml
@@ -228,14 +228,30 @@ and pp_branch_expr = function
 
 let pp_definition_expr = function
   | Vernacexpr.ProveBody (args, expr) ->
-      sequence
-        [
-          map_sequence
-            (fun arg -> sequence [ space; pp_local_binder_expr arg ])
-            args;
-          write " : ";
-          pp_constr_expr expr;
-        ]
+      let hor =
+        sequence
+          [
+            map_sequence
+              (fun arg -> sequence [ space; pp_local_binder_expr arg ])
+              args;
+            write " : ";
+            pp_constr_expr expr;
+          ]
+      in
+      let ver =
+        sequence
+          [
+            map_sequence
+              (fun arg -> sequence [ space; pp_local_binder_expr arg ])
+              args;
+            write " :";
+            newline;
+            increase_indent;
+            pp_constr_expr expr;
+            decrease_indent;
+          ]
+      in
+      hor <-|> ver
   | Vernacexpr.DefineBody (args, None, def_body, return_ty) ->
       sequence
         [
