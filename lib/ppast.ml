70ec822f286aebea76d9fbd18530a3d961bfa596
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-21 12:59:07 +0900

    Replace a wildcard

--- lib/ppast.ml
+++ lib/ppast.ml
@@ -403,7 +403,7 @@ let pp_induction_clause_list = function
   | _ -> fun printer -> raise (NotImplemented (contents printer))
 
 let pp_raw_atomic_tactic_expr = function
-  | Tacexpr.TacAssert (false, true, Some _, Some _, _) ->
+  | Tacexpr.TacAssert (false, true, Some None, Some _, _) ->
       write "assert (H : 1 = 1)."
   | Tacexpr.TacInductionDestruct (is_induction, false, clause_list) ->
       sequence
