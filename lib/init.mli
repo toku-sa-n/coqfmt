b32397b63d88be38c3956db25d0047dc81b95147
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-22 15:28:55 +0900

    Wrap in 80

--- lib/init.mli
+++ lib/init.mli
@@ -1,4 +1,5 @@
 val init_coq : unit -> unit
 (** Initialize the Coq environment and the handler of command line arguments.
-    
-    This function mult be called once and only once, and in the very early stage. *)
+
+    This function mult be called once and only once, and in the very early
+    stage. *)
