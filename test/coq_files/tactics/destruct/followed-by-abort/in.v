abb36bb722b2a60862c7a0e63f89c81af9f78c01
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-22 09:54:31 +0900

    Add a test

---
+++ test/coq_files/tactics/destruct/followed-by-abort/in.v
@@ -0,0 +1,1 @@
+Theorem foo:forall(n:nat),n=n. Proof. destruct n. -simpl. Abort. Definition bar:=3.
---
+++ test/coq_files/tactics/destruct/followed-by-abort/out.v
@@ -0,0 +1,8 @@
+Theorem foo : forall (n : nat), n = n.
+Proof.
+  destruct n.
+    - simpl.
+Abort.
+
+Definition bar :=
+  3.
