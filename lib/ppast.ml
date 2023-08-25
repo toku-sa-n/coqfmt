fbf1baa7911e5634fcb574e386286932d9ac04a5
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-25 15:05:50 +0900

    Extract the common part

--- lib/ppast.ml
+++ lib/ppast.ml
@@ -273,34 +273,19 @@ let pp_definition_expr = function
           hor <-|> ver;
         ]
   | Vernacexpr.DefineBody (args, None, def_body, return_ty) ->
-      let hor =
-        sequence
-          [
-            map_sequence
-              (fun arg -> sequence [ space; pp_local_binder_expr arg ])
-              args;
-            (match return_ty with
-            | None -> nop
-            | Some ty -> sequence [ write " : "; pp_constr_expr ty ]);
-            write " := ";
-            pp_constr_expr def_body;
-          ]
-      in
-      let ver =
-        sequence
-          [
-            map_sequence
-              (fun arg -> sequence [ space; pp_local_binder_expr arg ])
-              args;
-            (match return_ty with
-            | None -> nop
-            | Some ty -> sequence [ write " : "; pp_constr_expr ty ]);
-            write " :=";
-            newline;
-            indented (pp_constr_expr def_body);
-          ]
-      in
-      hor <-|> ver
+      let hor = sequence [ space; pp_constr_expr def_body ] in
+      let ver = sequence [ newline; indented (pp_constr_expr def_body) ] in
+      sequence
+        [
+          map_sequence
+            (fun arg -> sequence [ space; pp_local_binder_expr arg ])
+            args;
+          (match return_ty with
+          | None -> nop
+          | Some ty -> sequence [ write " : "; pp_constr_expr ty ]);
+          write " :=";
+          hor <-|> ver;
+        ]
   | _ -> fun printer -> raise (NotImplemented (contents printer))
 
 let pp_proof_end = function
