baf770c3db228588e8110b6320287b06e7c1b8bf
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-29 23:15:36 +0900

    Change the expected output

--- test/coq_files/break-long-lines/binary-operator/single/out.v
+++ test/coq_files/break-long-lines/binary-operator/single/out.v
@@ -1,7 +1,7 @@
 Theorem foo :
   forall (my_first_parameter my_second_parameter : nat),
-    my_first_parameter = my_second_parameter
-      -> my_first_parameter = my_second_parameter.
+    my_first_parameter = my_second_parameter ->
+    my_first_parameter = my_second_parameter.
 Proof.
   intros my_first_parameter my_second_parameter H.
   rewrite -> H.
