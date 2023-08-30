678be0b338cdff5f737868efaa52f159f3ad3b7c
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-30 20:56:25 +0900

    Update the test code

--- test/coq_files/expression/list/out.v
+++ test/coq_files/expression/list/out.v
@@ -1,3 +1,13 @@
 Notation "[ x ; .. ; y ]" := (cons x .. (cons y nil) ..).
 
-Definition foo := [1; 2; 3] ++ [4; 5] ++ [6].
+Definition list1 := [1].
+
+Definition list2 := [1;2].
+
+Definition list3 := [1;2;3].
+
+Definition list4 := [1;2;3;4].
+
+Definition list5 := [1;2;3;4;5].
+
+Definition list6 := [1;2;3;4;5;6].
