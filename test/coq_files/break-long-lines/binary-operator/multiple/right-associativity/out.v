eb8e0cd692d8dd57b346a2fda18fd055224cc87a
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-30 09:12:07 +0900

    Add a test

---
+++ test/coq_files/break-long-lines/binary-operator/multiple/right-associativity/out.v
@@ -0,0 +1,10 @@
+Theorem foo :
+  forall (tokyo osaka sapporo kobe fukuoka nagoya : nat),
+    tokyo = osaka
+      -> osaka = sapporo
+      -> sapporo = kobe
+      -> kobe = fukuoka
+      -> fukuoka = nagoya
+      -> nagoya = tokyo.
+Proof.
+Abort.
