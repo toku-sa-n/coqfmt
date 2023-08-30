d917bff70d5f73aad66e05fdff9dcc215eb4a28c
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-30 15:47:47 +0900

    Pp an arrow chain (#182)

---
+++ test/coq_files/break-long-lines/binary-operator/multiple/right-associativity/in.v
@@ -0,0 +1,3 @@
+Theorem foo:forall (tokyo osaka sapporo kobe fukuoka nagoya:nat), tokyo=osaka->osaka=sapporo->sapporo=kobe->kobe=fukuoka->fukuoka=nagoya->nagoya=tokyo.
+Proof.
+Abort.
---
+++ test/coq_files/break-long-lines/binary-operator/multiple/right-associativity/out.v
@@ -0,0 +1,7 @@
+Theorem foo :
+  forall (tokyo osaka sapporo kobe fukuoka nagoya : nat),
+    tokyo = osaka ->
+    osaka = sapporo ->
+    sapporo = kobe -> kobe = fukuoka -> fukuoka = nagoya -> nagoya = tokyo.
+Proof.
+Abort.
