908012ab57fd848e8ad81e9e46d041a29241a685
Hiroki Tokunaga <tokusan441@gmail.com>
2023-09-02 00:56:28 +0900

    Fix the test

--- test/coq_files/notation/inside-module/out.v
+++ test/coq_files/notation/inside-module/out.v
@@ -5,11 +5,12 @@ Module Foo.
     | S n', S m' => eqb n' m'
     | _, _ => false
     end.
-  
+
   Notation "x =? y" := (eqb x y) (at level 70) : nat_scope.
-  
-  Theorem foo : forall (x y : nat), x =? y.
+
+  Theorem foo : forall (x y : nat), (x =? y) = true.
   Proof.
+    intros x y.
     destruct (x =? y).
   Abort.
 End Foo.
