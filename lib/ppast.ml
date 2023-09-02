f94b95fee8a53ada399e6749a0d97740d0b5f5c4
Hiroki Tokunaga <tokusan441@gmail.com>
2023-09-02 22:43:07 +0900

    Merge two branches

--- lib/ppast.ml
+++ lib/ppast.ml
@@ -284,11 +284,15 @@ and pp_local_binder_expr = function
         Constrexpr.Default Explicit,
         CAst.{ v = Constrexpr.CHole (_, IntroAnonymous, None); loc = _ } ) ->
       pp_lname name
-  | Constrexpr.CLocalAssum (names, Constrexpr.Default Explicit, ty) ->
-      parens
-        (sequence [ map_spaced pp_lname names; write " : "; pp_constr_expr ty ])
-  | Constrexpr.CLocalAssum (names, Constrexpr.Default MaxImplicit, ty) ->
-      braces
+  | Constrexpr.CLocalAssum (names, Constrexpr.Default kind, ty) ->
+      let wrapper =
+        match kind with
+        | Explicit -> parens
+        | MaxImplicit -> braces
+        | _ -> fun _ printer -> raise (NotImplemented (contents printer))
+      in
+
+      wrapper
         (sequence [ map_spaced pp_lname names; write " : "; pp_constr_expr ty ])
   | _ -> fun printer -> raise (NotImplemented (contents printer))
 
