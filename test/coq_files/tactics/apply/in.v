4c575618121d5c187d385b9de56832a0e0de1251
Hiroki Tokunaga <tokusan441@gmail.com>
2023-09-03 13:33:53 +0900

    Add a test

---
+++ test/coq_files/tactics/apply/in.v
@@ -0,0 +1,1 @@
+Theorem foo:forall(n m:nat),n=m->n=m. Proof. intros n m eq. apply eq. Qed.
---
+++ test/coq_files/tactics/apply/out.v
@@ -0,0 +1,5 @@
+Theorem foo : forall (n m : nat), n = m -> n = m.
+Proof.
+  intros n m eq.
+  apply eq.
+Qed.
