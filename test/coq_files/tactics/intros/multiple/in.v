63ef6417c3d652285a85371037ed5cb5f08518fc
Hiroki Tokunaga <tokusan441@gmail.com>
2023-09-03 12:22:33 +0900

    Wrong files are committed

--- test/coq_files/tactics/intros/multiple/in.v
+++ test/coq_files/tactics/intros/multiple/in.v
@@ -1,3 +1,1 @@
-Fixpoint eqb (n m : nat) : bool := match n, m with | O, O => true | S n', S m' => eqb n' m' | _, _ => false end.
-Notation"x =? y":=(eqb x y)(at level 70):nat_scope.
-Theorem foo:forall n m:nat,n=?m=true. Proof. intros n m. Abort.
+Theorem foo:forall n m,n=m. Proof. intros n m. Abort.
--- test/coq_files/tactics/intros/multiple/out.v
+++ test/coq_files/tactics/intros/multiple/out.v
@@ -1,13 +1,4 @@
-Fixpoint eqb (n m : nat) : bool :=
-  match n, m with
-  | O, O => true
-  | S n', S m' => eqb n' m'
-  | _, _ => false
-  end.
-
-Notation "x =? y" := (eqb x y) (at level 70) : nat_scope.
-
-Theorem foo : forall (n m : nat), (n =? m) = true.
+Theorem foo : forall n m, n = m.
 Proof.
   intros n m.
 Abort.
