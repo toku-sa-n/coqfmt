e4094f3dbfb27775fad9a334edfa72dc1a43fa6b
Hiroki Tokunaga <tokusan441@gmail.com>
2023-09-04 12:18:58 +0900

    Replace a wildcard

--- lib/ppast.ml
+++ lib/ppast.ml
@@ -503,7 +503,7 @@ let pp_induction_clause_list = function
   | _ -> fun printer -> raise (NotImplemented (contents printer))
 
 let pp_hyp_location_expr = function
-  | (_, _), Locus.InHyp -> write "H"
+  | (Locus.AllOccurrences, _), Locus.InHyp -> write "H"
   | _ -> fun printer -> raise (NotImplemented (contents printer))
 
 let pp_raw_atomic_tactic_expr = function
