a01efe8956e04f68833368f51f095557c23dc4bc
Hiroki Tokunaga <tokusan441@gmail.com>
2023-09-04 12:14:25 +0900

    Pp func

--- lib/ppast.ml
+++ lib/ppast.ml
@@ -502,6 +502,8 @@ let pp_induction_clause_list = function
   | [ clause ], None -> pp_induction_clause clause
   | _ -> fun printer -> raise (NotImplemented (contents printer))
 
+let pp_hyp_location_expr _ = write "H"
+
 let pp_raw_atomic_tactic_expr = function
   | Tacexpr.TacApply (true, false, [ (None, (expr, binding)) ], []) ->
       let pp_binding =
@@ -550,9 +552,15 @@ let pp_raw_atomic_tactic_expr = function
   | Tacexpr.TacReduce (expr, { onhyps = Some []; concl_occs = AllOccurrences })
     ->
       sequence [ pp_raw_red_expr expr; write "." ]
-  | Tacexpr.TacReduce (expr, { onhyps = Some [ _ ]; concl_occs = NoOccurrences })
-    ->
-      sequence [ pp_raw_red_expr expr; write " in H." ]
+  | Tacexpr.TacReduce
+      (expr, { onhyps = Some [ name ]; concl_occs = NoOccurrences }) ->
+      sequence
+        [
+          pp_raw_red_expr expr;
+          write " in ";
+          pp_hyp_location_expr name;
+          write ".";
+        ]
   | Tacexpr.TacRewrite
       ( false,
         [ (is_left_to_right, Precisely 1, (None, (expr, NoBindings))) ],
