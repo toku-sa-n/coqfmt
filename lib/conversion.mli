823a78bbd4778798dc2a0ce18f7115b87f597725
Hiroki Tokunaga <tokusan441@gmail.com>
2023-09-04 10:51:57 +0900

    Consistency

--- lib/conversion.mli
+++ lib/conversion.mli
@@ -1,4 +1,4 @@
 val raw_tactic_expr_of_raw_generic_argument :
   Genarg.raw_generic_argument -> Ltac_plugin.Tacexpr.raw_tactic_expr option
-(** Converts the given value of type [Genarg.raw_generic_argument] to a value of
+(** Converts the given value of [Genarg.raw_generic_argument] to a value of
   [Ltac_plugin.Tacexpr.raw_tactic_expr]. *)
