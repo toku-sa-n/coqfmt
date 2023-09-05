3ceec9bfd463b0762f5b8968f60a0e733153e254
Hiroki Tokunaga <tokusan441@gmail.com>
2023-09-05 23:33:34 +0900

    Fix the test

--- test/coq_files/tactics/destruct/multiple-bullets/in.v
+++ test/coq_files/tactics/destruct/multiple-bullets/in.v
@@ -1,1 +1,1 @@
-Theorem foo:forall (a b c:bool),a=b->b=c. Proof. destruct a. --destruct b. ++destruct c. ** simpl. Abort.
+Theorem foo:forall (a b c:bool),a=b->b=c. Proof. destruct a. --destruct b. +++destruct c. **** simpl. Abort.
--- test/coq_files/tactics/destruct/multiple-bullets/out.v
+++ test/coq_files/tactics/destruct/multiple-bullets/out.v
@@ -2,6 +2,6 @@ Theorem foo : forall (a b c : bool), a = b -> b = c.
 Proof.
   destruct a.
     -- destruct b.
-         ++ destruct c.
-              ** simpl.
+         +++ destruct c.
+               **** simpl.
 Abort.
