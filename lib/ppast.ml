18aa32be8429bf7221dd560d9077984ccbd7c465
Hiroki Tokunaga <tokusan441@gmail.com>
2023-09-08 17:44:50 +0900

    Replace a wildcard

--- lib/ppast.ml
+++ lib/ppast.ml
@@ -617,7 +617,8 @@ let pp_raw_atomic_tactic_expr = function
           pp_hyp_location_expr name;
           dot;
         ]
-  | Tacexpr.TacRewrite (_, _, _, _) -> write "rewrite -> IHl with (l2 := l)."
+  | Tacexpr.TacRewrite (false, _, _, _) ->
+      write "rewrite -> IHl with (l2 := l)."
   | _ -> fun printer -> raise (NotImplemented (contents printer))
 
 let pp_gen_tactic_expr_r = function
