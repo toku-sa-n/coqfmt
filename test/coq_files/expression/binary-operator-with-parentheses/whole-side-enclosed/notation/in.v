c9c4021c7590cfc8762b9615491795a298bc99ec
Hiroki Tokunaga <tokusan441@gmail.com>
2023-09-09 10:58:16 +0900

    Add a test

---
+++ test/coq_files/expression/binary-operator-with-parentheses/whole-side-enclosed/notation/in.v
@@ -0,0 +1,1 @@
+Theorem restricted_excluded_middle:forall(P b:Prop),(P<->b=true)->P\/~P. Abort.
---
+++ test/coq_files/expression/binary-operator-with-parentheses/whole-side-enclosed/notation/out.v
@@ -0,0 +1,2 @@
+Theorem restricted_excluded_middle :
+  forall (P b : Prop), (P <-> b = true) -> P \/ ~ P.
