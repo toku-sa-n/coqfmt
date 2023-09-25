ac7f954b963301df7324802934d39e66ca3d2b5a
Hiroki Tokunaga <tokusan441@gmail.com>
2023-09-25 11:01:53 +0900

    Remove the unnecessary sequence

--- lib/ppast.ml
+++ lib/ppast.ml
@@ -459,7 +459,7 @@ let pp_syntax_modifier = function
 let rec pp_gen_tactic_arg = function
   | Tacexpr.ConstrMayEval (ConstrTerm expr) -> pp_constr_expr expr
   | Tacexpr.Reference name -> pp_qualid name
-  | Tacexpr.TacCall CAst.{ v = v, []; loc = _ } -> sequence [ pp_qualid v ]
+  | Tacexpr.TacCall CAst.{ v = v, []; loc = _ } -> pp_qualid v
   | Tacexpr.TacCall CAst.{ v = name, [ arg ]; loc = _ } ->
       sequence [ pp_qualid name; space; pp_gen_tactic_arg arg ]
   | _ -> fun printer -> raise (NotImplemented (contents printer))
