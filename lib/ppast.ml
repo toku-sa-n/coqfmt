7b0253564d39e566f4336ca0cf7a76561522cbb6
Hiroki Tokunaga <tokusan441@gmail.com>
2023-10-05 20:00:04 +0900

    Pass the test

--- lib/ppast.ml
+++ lib/ppast.ml
@@ -116,8 +116,9 @@ and pp_constr_expr_r = function
   | Constrexpr.CAppExpl ((dots, None), [ expr ])
     when Libnames.string_of_qualid dots = ".." ->
       spaced [ pp_qualid dots; parens (pp_constr_expr expr); pp_qualid dots ]
-  | Constrexpr.CAppExpl ((name, None), [ expr ]) ->
-      sequence [ write "@"; pp_qualid name; space; pp_constr_expr expr ]
+  | Constrexpr.CAppExpl ((name, None), exprs) ->
+      sequence
+        [ write "@"; pp_qualid name; space; map_spaced pp_constr_expr exprs ]
   | Constrexpr.CCases (_, None, matchees, branches) ->
       sequence
         [
