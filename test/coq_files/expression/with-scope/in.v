1bf75ad36eaa3cdc87c26856d51f4cc6d9c07028
Hiroki Tokunaga <tokusan441@gmail.com>
2023-10-14 15:57:41 +0900

    Add a test

--- test/coq_files/expression/with-scope/in.v
+++ test/coq_files/expression/with-scope/in.v
@@ -1,1 +1,1 @@
-Require Export Coq.Strings.String. Check String.eqb_refl%string.
+Require Export Coq.Strings.String. Check String.eqb_refl%string. Check String.eqb_refl:forall(x:string),(x=?x)%string=true.
