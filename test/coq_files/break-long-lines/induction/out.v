ec55ad8fab7510a5b9e4afeda6ae7224bedbf033
Hiroki Tokunaga <tokusan441@gmail.com>
2023-10-22 18:35:49 +0900

    Pp a long induction clause (#284)

---
+++ test/coq_files/break-long-lines/induction/out.v
@@ -0,0 +1,15 @@
+Theorem foo : 1 = 1.
+Proof.
+  Fail induction foo as
+    [derich rosemary | ester menyanya shinobu |  | takoyaki ramen].
+  Fail induction foo as
+    [
+    | derich rosemary
+    | ester menyanya shinobu
+    |
+    | dina alfred
+    | zenyatta iris
+    | mao marion
+    |
+    ].
+Abort.
