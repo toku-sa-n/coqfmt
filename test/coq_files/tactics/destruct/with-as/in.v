aced95a9732ad8ea8b8b33dc087ba71b450705a5
Hiroki Tokunaga <tokusan441@gmail.com>
2023-10-24 08:55:45 +0900

    Simplify a test

--- test/coq_files/tactics/destruct/with-as/in.v
+++ test/coq_files/tactics/destruct/with-as/in.v
@@ -1,4 +1,1 @@
-Inductive bin : Type := | Z | B_0 (n : bin) | B_1 (n : bin).
-Fixpoint bin_to_nat (m : bin) : nat := match m with | Z => 0 | B_0 n => 2 * bin_to_nat n | B_1 n => 2 * bin_to_nat n + 1 end.
-Fixpoint incr (m : bin) : bin := match m with | Z => B_1 Z | B_0 n => B_1 n | B_1 n => B_0 (incr n) end.
-Theorem bin_to_nat_pres_incrr : forall (b : bin), bin_to_nat (incr b) = 1 + bin_to_nat b.  Proof.  intros b.  induction b as [ | b' IHb'| b' IHb']. Abort.
+Theorem foo:forall (n:nat),n=n. Proof. intros n. destruct n as [|n']. - reflexivity. - reflexivity. Qed.
