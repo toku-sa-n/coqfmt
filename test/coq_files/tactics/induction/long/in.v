0a1d7058d4ff2a76367b70707af9f8e5fa06f3f4
Hiroki Tokunaga <tokusan441@gmail.com>
2023-10-24 09:00:24 +0900

    Remove an unnecessary case

--- test/coq_files/tactics/induction/long/in.v
+++ test/coq_files/tactics/induction/long/in.v
@@ -1,6 +1,4 @@
 Theorem foo : 1 = 1.
 Proof.
   Fail induction foo as [derich rosemary | ester menyanya shinobu |  | takoyaki ramen].
-  Fail induction foo as
-    [ | derich rosemary | ester menyanya shinobu | | dina alfred | zenyatta iris | mao marion | ].
 Abort.
