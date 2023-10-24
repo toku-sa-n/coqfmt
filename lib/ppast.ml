0acebc20bc700032b2d20f8c2dd29ca6be408815
Hiroki Tokunaga <tokusan441@gmail.com>
2023-10-24 13:58:49 +0900

    Insert a newline before the return type of a `Definition` (#289)

--- lib/ppast.ml
+++ lib/ppast.ml
@@ -372,19 +372,35 @@ let pp_definition_expr = function
           hor <-|> ver;
         ]
   | Vernacexpr.DefineBody (args, None, def_body, return_ty) ->
-      let hor = sequence [ space; pp_constr_expr def_body ] in
-      let ver = sequence [ newline; indented (pp_constr_expr def_body) ] in
-      sequence
-        [
-          map_sequence
-            (fun arg -> sequence [ space; pp_local_binder_expr arg ])
-            args;
-          (match return_ty with
-          | None -> nop
-          | Some ty -> sequence [ write " : "; pp_constr_expr ty ]);
-          write " :=";
-          hor <-|> ver;
-        ]
+      let pp_args =
+        map_sequence
+          (fun arg -> sequence [ space; pp_local_binder_expr arg ])
+          args
+      in
+
+      let pp_return_ty =
+        match return_ty with
+        | None -> nop
+        | Some ty ->
+            let hor = sequence [ write " : "; pp_constr_expr ty ] in
+            let ver =
+              sequence
+                [
+                  newline; indented (sequence [ write ": "; pp_constr_expr ty ]);
+                ]
+            in
+
+            hor <-|> ver
+      in
+
+      let pp_body =
+        let hor = sequence [ space; pp_constr_expr def_body ] in
+        let ver = sequence [ newline; indented (pp_constr_expr def_body) ] in
+
+        hor <-|> ver
+      in
+
+      sequence [ pp_args; pp_return_ty; write " :="; pp_body ]
   | _ -> fun printer -> raise (NotImplemented (contents printer))
 
 let pp_proof_end = function
