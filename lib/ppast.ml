718b9a0e5a819efff7d2fd822be308d592f93ba0
Hiroki Tokunaga <tokusan441@gmail.com>
2023-10-24 14:08:38 +0900

    Replace a wildcard

--- lib/ppast.ml
+++ lib/ppast.ml
@@ -939,7 +939,8 @@ let pp_notation_declaration = function
   | _ -> fun printer -> raise (NotImplemented (contents printer))
 
 let pp_comment = function
-  | Vernacexpr.CommentConstr _ -> write "Comments prev."
+  | Vernacexpr.CommentConstr expr ->
+      sequence [ write "Comments "; pp_constr_expr expr; dot ]
   | Vernacexpr.CommentString s ->
       sequence [ write "Comments "; doublequoted (write s); dot ]
   | _ -> fun printer -> raise (NotImplemented (contents printer))
