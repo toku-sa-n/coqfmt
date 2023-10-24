236144ec380df3b3ef08c30c97428c214a332c1b
Hiroki Tokunaga <tokusan441@gmail.com>
2023-10-24 09:41:45 +0900

    Separate `or_and_intropattern` tests (#285)

---
+++ test/coq_files/or_and_intropattern/long/in.v
@@ -0,0 +1,4 @@
+Theorem foo:1=1.
+  Fail induction foo as
+    [ | derich rosemary | ester menyanya shinobu | | dina alfred | zenyatta iris | mao marion | ].
+Abort.
---
+++ test/coq_files/tactics/induction/long/out.v
@@ -0,0 +1,5 @@
+Theorem foo : 1 = 1.
+Proof.
+  Fail induction foo as
+    [derich rosemary | ester menyanya shinobu |  | takoyaki ramen].
+Abort.
