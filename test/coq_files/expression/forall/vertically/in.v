0d53cc50d73b10fcc9cdfa285f524b9cdccea6d3
Hiroki Tokunaga <tokusan441@gmail.com>
2023-10-28 18:38:33 +0900

    Pp `forall` parameters vertically (#310)

---
+++ test/coq_files/expression/forall/vertically/in.v
@@ -0,0 +1,3 @@
+Check
+  (forall (a : nat) (b : nat) (c : nat) (d : nat) (e : nat) (f : nat) (g : nat) (h : nat),
+     True).
---
+++ test/coq_files/expression/forall/vertically/out.v
@@ -0,0 +1,10 @@
+Check
+  (forall
+     (a : nat)
+     (b : nat)
+     (c : nat)
+     (d : nat)
+     (e : nat)
+     (f : nat)
+     (g : nat)
+     (h : nat), True).
