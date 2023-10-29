7adadb87ceb21050285572541c882454e07545da
Hiroki Tokunaga <tokusan441@gmail.com>
2023-10-30 08:53:25 +0900

    Add a test

---
+++ test/coq_files/command/semicolon/vertically/in.v
@@ -0,0 +1,4 @@
+Theorem foo : 1 = 1.
+Proof.
+  simpl; simpl; simpl; simpl; simpl; simpl; simpl; simpl; simpl; simpl; reflexivity.
+Qed.
---
+++ test/coq_files/command/semicolon/vertically/out.v
@@ -0,0 +1,14 @@
+Theorem foo : 1 = 1.
+Proof.
+  simpl;
+    simpl;
+    simpl;
+    simpl;
+    simpl;
+    simpl;
+    simpl;
+    simpl;
+    simpl;
+    simpl;
+    reflexivity.
+Qed.
