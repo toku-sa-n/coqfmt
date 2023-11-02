3295c885c3cc70381eecde6ff8ccbc1a09a19dd4
Hiroki Tokunaga <t1810441@edu.cc.uec.ac.jp>
2023-11-02 12:56:11 +0900

    Add a test

---
+++ test/coq_files/tactics/try/multiple-lines/out.v
@@ -0,0 +1,18 @@
+Theorem foo : 1 = 1.
+Proof.
+  try
+    (simpl;
+       simpl;
+       simpl;
+       simpl;
+       simpl;
+       simpl;
+       simpl;
+       simpl;
+       simpl;
+       simpl;
+       simpl;
+       simpl;
+       simpl;
+       reflexivity).
+Qed.
