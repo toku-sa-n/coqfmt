362c9519547493260fab1f52c1cb8e853e9be8d8
Hiroki Tokunaga <tokusan441@gmail.com>
2023-11-06 14:11:59 +0900

    Pass the test

--- lib/conversion.mli
+++ lib/conversion.mli
@@ -3,6 +3,11 @@ val raw_tactic_expr_of_raw_generic_argument :
 (** Convert the given value of [Genarg.raw_generic_argument] to a value of
   [Ltac_plugin.Tacexpr.raw_tactic_expr]. *)
 
+val tacdef_body_of_raw_generic_argument :
+  Genarg.raw_generic_argument -> Ltac_plugin.Tacexpr.tacdef_body list option
+(** Convert the given value of [Genarg.raw_generic_argument] to a value of
+  [Ltac_plugin.Tacexpr.tacdef_body list]. *)
+
 val constr_expr_of_raw_generic_argument :
   Genarg.raw_generic_argument -> Constrexpr.constr_expr option
 (** Convert the given value of [Genarg.raw_generic_argument] to a value of
