9bc17255634eb293136f62a4a4ff80b1691b17d9
Hiroki Tokunaga <tokusan441@gmail.com>
2023-11-15 14:48:52 +0900

    Destruct a wildcard

--- lib/ppast.ml
+++ lib/ppast.ml
@@ -603,7 +603,7 @@ let pp_syntax_modifier = function
         ]
   | Vernacexpr.SetItemLevel ([ name ], None, NextLevel) ->
       sequence [ write name; write " at next level" ]
-  | Vernacexpr.SetItemLevel _ -> write "x at level 0"
+  | Vernacexpr.SetItemLevel (_, _, _) -> write "x at level 0"
   | Vernacexpr.SetLevel level ->
       sequence [ write "at level "; write (string_of_int level) ]
   | _ -> fun printer -> raise (NotImplemented (contents printer))
