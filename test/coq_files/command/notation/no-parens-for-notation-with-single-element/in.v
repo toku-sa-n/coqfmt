742c43ef3f8959df326a418709a2659788f92f73
Hiroki Tokunaga <tokusan441@gmail.com>
2023-11-16 19:37:21 +0900

    Add a test

--- test/coq_files/command/notation/no-parens-for-notation-with-single-element/in.v
+++ test/coq_files/command/notation/no-parens-for-notation-with-single-element/in.v
@@ -2,6 +2,10 @@ Declare Custom Entry com.
 Declare Scope com_scope.
 
 Notation "<{ x }>" := x (x custom com at level 99) : com_scope.
+Notation "( x )" := x (in custom com, x at level 99) : com_scope.
+Notation "x + y" := (plus x y) (in custom com at level 50, left associativity) : com_scope.
 Notation "x" := x (in custom com at level 0, x constr at level 0) : com_scope.
 
 Fail Definition foo := <{ 1 }>.
+
+Fail Definition bar := <{3 + (1 + 2)}>.
