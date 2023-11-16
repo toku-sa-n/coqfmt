95fecd84a890b02f2358761123e0922a06b6c767
Hiroki Tokunaga <tokusan441@gmail.com>
2023-11-16 17:38:18 +0900

    Do not duplicate parentheses (#338)

---
+++ test/coq_files/command/notation/no-parens-duplication/out.v
@@ -0,0 +1,7 @@
+Declare Custom Entry com.
+Declare Scope com_scope.
+
+Notation "<{ x }>" := x (x custom com at level 99) : com_scope.
+Notation "x" := x (in custom com at level 0, x constr at level 0) : com_scope.
+
+Fail Definition foo := <{ 1 }>.
