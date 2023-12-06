669d46b6e36a136d99c92e5217f8de972366f9b8
Hiroki Tokunaga <tokusan441@gmail.com>
2023-12-06 12:40:38 +0900

    Rename

--- lib/ppast.ml
+++ lib/ppast.ml
@@ -1686,8 +1686,8 @@ let pp_synpure_vernac_expr = function
       sequence [ bullet_appears bullet; pp_proof_bullet bullet ]
   | Vernacexpr.VernacSubproof None -> sequence [ write "{"; start_subproof ]
   | Vernacexpr.VernacEndSubproof -> sequence [ end_subproof; write "}" ]
-  | Vernacexpr.VernacHints ([ scope ], expr) ->
-      sequence [ pp_hints_expr expr; write " : "; write scope; dot ]
+  | Vernacexpr.VernacHints ([ database ], expr) ->
+      sequence [ pp_hints_expr expr; write " : "; write database; dot ]
   | _ -> fun printer -> raise (NotImplemented (contents printer))
 
 let pp_vernac_expr = function
