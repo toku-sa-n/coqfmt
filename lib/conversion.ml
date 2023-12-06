a3a0e75529ddf093d1f0887c56bd8d8ee9eba1e9
Hiroki Tokunaga <tokusan441@gmail.com>
2023-12-06 15:53:43 +0900

    Pass the test

--- lib/conversion.ml
+++ lib/conversion.ml
@@ -177,3 +177,14 @@ let auto_using_of_raw_generic_argument arg =
       ] ->
       Some (List.map Serlib.Ser_constrexpr.constr_expr_of_sexp rems)
   | _ -> None
+
+let ltac_of_raw_generic_argument arg =
+  match Serlib.Ser_genarg.sexp_of_raw_generic_argument arg with
+  | List
+      [
+        Atom "GenArg";
+        List [ Atom "Rawwit"; List [ Atom "ExtraArg"; Atom "ltac" ] ];
+        rems;
+      ] ->
+      Some (Serlib_ltac.Ser_tacexpr.raw_tactic_expr_of_sexp rems)
+  | _ -> None
--- lib/conversion.mli
+++ lib/conversion.mli
@@ -54,3 +54,8 @@ val auto_using_of_raw_generic_argument :
   Genarg.raw_generic_argument -> Constrexpr.constr_expr list option
 (** Convert the given value of [Genarg.raw_generic_argument] to a value of
   [Constrexpr.constr_expr list]. *)
+
+val ltac_of_raw_generic_argument :
+  Genarg.raw_generic_argument -> Ltac_plugin.Tacexpr.raw_tactic_expr option
+(** Convert the given value of [Genarg.raw_generic_argument] to a value of
+  [Ltac_plugin.Tacexpr.raw_tactic_expr]. *)
