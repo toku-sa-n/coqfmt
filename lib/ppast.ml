22769e0dbfe35fc77770b6d893a5bca55f7cea3e
Hiroki Tokunaga <tokusan441@gmail.com>
2023-12-08 12:57:01 +0900

    Destruct a wildcard

--- lib/ppast.ml
+++ lib/ppast.ml
@@ -1409,7 +1409,7 @@ let pp_reference_or_constr = function
       fun printer -> raise (NotImplemented (contents printer))
 
 let pp_hints_expr = function
-  | Vernacexpr.HintsConstructors _ -> write "Hint Constructors list"
+  | Vernacexpr.HintsConstructors [ _ ] -> write "Hint Constructors list"
   | Vernacexpr.HintsResolve
       [ ({ hint_priority = None; hint_pattern = None }, true, expr) ] ->
       sequence [ write "Hint Resolve "; pp_reference_or_constr expr ]
