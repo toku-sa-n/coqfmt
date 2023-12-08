a614399d77b190d6bcd83ddddab13047be1aa1ad
Hiroki Tokunaga <tokusan441@gmail.com>
2023-12-08 13:26:06 +0900

    Replace a wildcard

--- lib/ppast.ml
+++ lib/ppast.ml
@@ -1414,7 +1414,8 @@ let pp_hints_expr = function
   | Vernacexpr.HintsResolve
       [ ({ hint_priority = None; hint_pattern = None }, true, expr) ] ->
       sequence [ write "Hint Resolve "; pp_reference_or_constr expr ]
-  | Vernacexpr.HintsTransparency (_, true) -> write "Hint Transparent x"
+  | Vernacexpr.HintsTransparency (HintsReferences _, true) ->
+      write "Hint Transparent x"
   | Vernacexpr.HintsUnfold [ name ] ->
       sequence [ write "Hint Unfold "; pp_qualid name ]
   | _ -> fun printer -> raise (NotImplemented (contents printer))
