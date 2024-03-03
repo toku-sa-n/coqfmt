e922965373c8a6595bce075c179e4e000ad54ff9
Hiroki Tokunaga <tokusan441@gmail.com>
2024-03-04 08:36:04 +0900

    Insert empty lines around `Extract Inductive`s (#424)

--- test/coq_files/empty-line-for-separation/extraction/out.v
+++ test/coq_files/empty-line-for-separation/extraction/out.v
@@ -9,3 +9,5 @@ Extract Constant mult => "( * )".
 
 Extract Inductive bool => "bool" ["true" "false"].
 Extract Inductive nat => "int" ["0" "(fun x->x+1)"] "(fun zero succ n->if n=0 then zero() else succ (n-1))".
+
+From Coq Require Import ZArith.
