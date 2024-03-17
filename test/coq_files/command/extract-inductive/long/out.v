741552fe5bcec78d12ebe35c7574e2c3e870358b
Hiroki Tokunaga <tokusan441@gmail.com>
2024-03-17 11:03:21 +0900

    Fix the test

--- test/coq_files/command/extract-inductive/long/out.v
+++ test/coq_files/command/extract-inductive/long/out.v
@@ -1,4 +1,4 @@
 Require Coq.extraction.Extraction.
 
 Extract Inductive nat => "int" ["0" "(fun x->x+1)"]
-    "(fun zero succ n->if n=0 then zero() else succ (n-1))".
+  "(fun zero succ n->if n=0 then zero() else succ (n-1))".
