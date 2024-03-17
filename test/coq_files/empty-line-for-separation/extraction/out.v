c80a46b07181edf5999d9af9692e16a8a026d4f4
Hiroki Tokunaga <tokusan441@gmail.com>
2024-03-17 11:04:54 +0900

    Fix tests

--- test/coq_files/empty-line-for-separation/extraction/out.v
+++ test/coq_files/empty-line-for-separation/extraction/out.v
@@ -8,6 +8,7 @@ Extract Constant plus => "( + )".
 Extract Constant mult => "( * )".
 
 Extract Inductive bool => "bool" ["true" "false"].
-Extract Inductive nat => "int" ["0" "(fun x->x+1)"] "(fun zero succ n->if n=0 then zero() else succ (n-1))".
+Extract Inductive nat => "int" ["0" "(fun x->x+1)"]
+  "(fun zero succ n->if n=0 then zero() else succ (n-1))".
 
 From Coq Require Import ZArith.
