0051e9515577b27aae3648c19ec0a9bf70a739b4
Hiroki Tokunaga <tokusan441@gmail.com>
2024-03-18 12:11:10 +0900

    Add a test

---
+++ test/coq_files/tactics/clear/multiple/in.v
@@ -0,0 +1,1 @@
+Theorem foo:1=1. Proof. assert(H:1=1) by reflexivity. assert(I:1=1) by reflexivity. clear H I. Abort.
---
+++ test/coq_files/tactics/clear/multiple/out.v
@@ -0,0 +1,6 @@
+Theorem foo : 1 = 1.
+Proof.
+  assert (H : 1 = 1) by reflexivity.
+  assert (I : 1 = 1) by reflexivity.
+  clear H I.
+Abort.
