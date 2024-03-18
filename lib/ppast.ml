fa092872fb744d639bf4efea7fb8fc18eb324a5a
Hiroki Tokunaga <tokusan441@gmail.com>
2024-03-18 11:52:06 +0900

    Fixes #431 (#432)

--- lib/ppast.ml
+++ lib/ppast.ml
@@ -370,32 +370,31 @@ and pp_constr_expr_r = function
         | Ppextend.UnpBinderMetaVar _ :: _, _, _, _, _ ->
             failwith "Too few entry keys."
         | Ppextend.UnpListMetaVar (_, seps) :: t, elems, _, _ :: zs, _ ->
-            let get_sep = function
-              | Ppextend.UnpTerminal s -> s
-              | Ppextend.UnpCut (PpBrk _) -> ";"
-              | _ -> failwith "TODO"
+            let sep =
+              let cond = function
+                | Ppextend.UnpTerminal s -> Some s
+                | _ -> None
+              in
+
+              match List.find_map cond seps with Some s -> s | None -> ""
             in
-            let rec loop elems seps =
-              match (elems, seps) with
-              | [ x ], _ ->
+
+            let loop elems =
+              match elems with
+              | [ x ] ->
                   sequence
                     [
                       pp_constr_expr x; pp_generic t [] local_assums zs patterns;
                     ]
-              | [], _ -> pp_generic t [] local_assums zs patterns
-              | xs, [] ->
+              | [] -> pp_generic t [] local_assums zs patterns
+              | xs ->
                   sequence
                     [
-                      map_with_seps ~sep:(write "; ") pp_constr_expr xs;
+                      map_with_seps ~sep:(write (sep ^ " ")) pp_constr_expr xs;
                       pp_generic t [] local_assums zs patterns;
                     ]
-              | x :: xs, sep :: seps ->
-                  sequence
-                    [
-                      pp_constr_expr x; write (get_sep sep); space; loop xs seps;
-                    ]
             in
-            loop elems seps
+            loop elems
         | Ppextend.UnpListMetaVar (_, _) :: _, _, _, [], _ ->
             raise (NotImplemented "")
         | Ppextend.UnpBinderListMetaVar _ :: _, _, _, [], _ ->
