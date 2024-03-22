4b0decc953743e04931fc59b5c49a68457056182
Hiroki Tokunaga <tokusan441@gmail.com>
2024-03-22 23:42:26 +0900

    Replace a wildcard

--- lib/ppast.ml
+++ lib/ppast.ml
@@ -1901,8 +1901,8 @@ let pp_synpure_vernac_expr = function
           pp_definition_expr expr;
           dot;
         ]
-  | Vernacexpr.VernacDelimiters (_, _) ->
-      write "Delimit Scope nat_scope with pred."
+  | Vernacexpr.VernacDelimiters (scope, _) ->
+      sequence [ write "Delimit Scope "; write scope; write " with pred." ]
   | Vernacexpr.VernacCoercion (CAst.{ v = AN name; loc = _ }, Some (src, dst))
     ->
       sequence
