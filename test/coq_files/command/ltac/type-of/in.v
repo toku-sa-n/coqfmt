35358797777ecd07030cf067c97e9ed16d462b46
Hiroki Tokunaga <tokusan441@gmail.com>
2024-04-29 21:47:08 +0900

    Add a test

--- test/coq_files/command/ltac/type-of/in.v
+++ test/coq_files/command/ltac/type-of/in.v
@@ -1,1 +1,1 @@
-Ltac foo:=match goal with |H:?T|-_=>match type of T with _=>idtac end end.
+Ltac foo:=match goal with |H:?T|-_=>match type of T with Prop=>idtac "Prop"|_=>idtac end end.
--- test/coq_files/command/ltac/type-of/out.v
+++ test/coq_files/command/ltac/type-of/out.v
@@ -2,6 +2,7 @@ Ltac foo :=
   match goal with
   | H : ?T |- _ =>
     match type of T with
+    | Prop => idtac "Prop"
     | _ => idtac
     end
   end.
