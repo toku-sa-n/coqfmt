18d3bbaec587945868ea4025445f274cc75748b4
Hiroki Tokunaga <tokusan441@gmail.com>
2024-05-04 18:25:04 +0900

    Add a test

---
+++ test/coq_files/tactics/replace/by/long/in.v
@@ -0,0 +1,2 @@
+Theorem really_awfully_extremely_unnecessarily_too_very_long_name_for_replacing:(1=1)=(1=1). Proof. auto. Qed.
+Theorem foo:1=1. Proof. replace (1=1) with (1=1) by (apply really_awfully_extremely_unnecessarily_too_very_long_name_for_replacing). Abort.
