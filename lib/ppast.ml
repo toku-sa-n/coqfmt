76c16089df6e57cc4d75352162130748085570be
Hiroki Tokunaga <tokusan441@gmail.com>
2024-05-07 20:57:03 +0900

    Format

--- lib/ppast.ml
+++ lib/ppast.ml
@@ -1355,8 +1355,13 @@ and pp_raw_tactic_expr_r = function
         brackets (sequence (List.mapi pp_patterns second))
       in
 
-      let hor= sequence [ pp_raw_tactic_expr first; write "; "; pp_bracket_clause ] in
-      let ver=sequence [pp_raw_tactic_expr first;write ";";newline;pp_bracket_clause] in
+      let hor =
+        sequence [ pp_raw_tactic_expr first; write "; "; pp_bracket_clause ]
+      in
+      let ver =
+        sequence
+          [ pp_raw_tactic_expr first; write ";"; newline; pp_bracket_clause ]
+      in
 
       hor <-|> ver
   | Tacexpr.TacTry tactic ->
