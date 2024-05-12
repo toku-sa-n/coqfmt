a8abf9c0126b24a5b8086df46dfc72a0184776b4
Hiroki Tokunaga <tokusan441@gmail.com>
2024-05-12 12:14:15 +0900

    Fix the test

--- test/coq_files/tactics/thens/out.v
+++ test/coq_files/tactics/thens/out.v
@@ -1,6 +1,7 @@
 Theorem foo : forall (n : nat), n = n.
 Proof.
   destruct n;
-  [simpl; simpl; simpl; simpl; simpl; simpl; simpl; simpl; simpl; simpl | simpl;
-   simpl; simpl; simpl; simpl; simpl; simpl; simpl; simpl; simpl]; reflexivity.
+  [ simpl; simpl; simpl; simpl; simpl; simpl; simpl; simpl; simpl; simpl
+  | simpl; simpl; simpl; simpl; simpl; simpl; simpl; simpl; simpl; simpl];
+  reflexivity.
 Qed.
