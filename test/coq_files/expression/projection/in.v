b76b974d05b29ad51dacefc1c26ce981e8122ada
Wolf Honore <wolfhonore@gmail.com>
2024-06-04 21:03:36 -0400

    PP record projections (#503)

---
+++ test/coq_files/expression/projection/in.v
@@ -0,0 +1,1 @@
+Record R X {Y} := { rx: X; ry: Y }. Definition x1 (r: @R unit unit) := r.( rx  unit  ) . Definition x2 r := r .( @ rx). Definition x3 r := r.(@rx unit unit) . Definition x4 r := r.(rx  unit   (  Y:=unit) ).
